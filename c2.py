eb18f914ddcc3845a32cba637cdafd871a41364b
sanket0610 <sanketkingz10@gmail.com>
2025-07-03 19:12:50 +0530

    Add files via upload

---
+++ c2.py
@@ -0,0 +1,18 @@
+
+print("My name is Raj")
+print("These are my friends' birthdays")
+
+SanketVAR="June-10th"
+print("Sanket's birthday is on", SanketVAR)
+
+SajalVAR="July-8th"
+print("Sajal's birthday is on", SajalVAR)
+
+VivanVAR="March-19th"
+print("Vivan's birthday is on", VivanVAR)
+
+JanmeshVAR="December-16th"
+print("Janmesh's birthday is on", JanmeshVAR)
+
+RajatVAR="May-18th"
+print("Rajat's birthday is on", RajatVAR)
